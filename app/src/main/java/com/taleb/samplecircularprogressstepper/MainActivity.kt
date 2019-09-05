8103aa50ddd49d2b2840a20c8196929005809bf3
Taleb <taleb.r75@gmail.com>
2019-09-05 20:55:01 +0430

    improve minor things

--- app/src/main/java/com/taleb/samplecircularprogressstepper/MainActivity.kt
+++ app/src/main/java/com/taleb/samplecircularprogressstepper/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.taleb.samplecircularprogressstepper
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 import android.graphics.Color
+import android.os.Bundle
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 import nl.dionsegijn.steppertouch.OnStepCallback
 
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //cpsStepper.setStepsImgSrc(stepsSrc)
+        cpsStepper.setStepsImgSrc(stepsSrc)
 
         numOfSteps = cpsStepper.getNumOfSteps()
         numOfStepStepperTouch.count = cpsStepper.getNumOfSteps()
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             override fun onStep(value: Int, positive: Boolean) {
                 numOfSteps = value
                 currentStepStepperTouch.maxValue = numOfSteps
-                cpsStepper.setNumOfSeps(numOfSteps)
+                cpsStepper.setNumOfSteps(numOfSteps)
                 Toast.makeText(applicationContext, value.toString(), Toast.LENGTH_SHORT).show()
             }
         })
@@ -47,8 +47,30 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        progressColorTV.text = String.format("#%06X", 0xFFFFFF and cpsStepper.getProgressColor())
+        progressColorV.setBackgroundColor(cpsStepper.getProgressColor())
+        progressColorTV.setOnClickListener {
+            showColorPicker(progressColorTV.text.toString()){
+                cpsStepper.setProgressColor(it)
+                progressColorTV.text = String.format("#%06X", 0xFFFFFF and it)
+                progressColorV.setBackgroundColor(it)
+            }
+        }
+        foregroundColorTV.text = String.format("#%06X", 0xFFFFFF and cpsStepper.getForegroundColor())
+        foregroundColorV.setBackgroundColor(cpsStepper.getForegroundColor())
+        foregroundColorTV.setOnClickListener {
+            showColorPicker(foregroundColorTV.text.toString()){
+                cpsStepper.setForegroundColor(it)
+                foregroundColorTV.text = String.format("#%06X", 0xFFFFFF and it)
+                foregroundColorV.setBackgroundColor(it)
+            }
+        }
+
+
+
         //
         progressStrokeOffsetTV.text = cpsStepper.getProgressStrokeOffset().toString()+" dp"
+        progressStrokeOffsetSeekBar.progress  = (cpsStepper.getProgressStrokeOffset()/resources.displayMetrics.density).toInt()
         progressStrokeOffsetSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 val offsetInDp = (progress*resources.displayMetrics.density).toInt()
@@ -65,19 +87,39 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        //
+        progressStepSizeTV.text = cpsStepper.getStepsSize().toString()+" dp"
+        progressStepSizeSeekBar.progress = (cpsStepper.getStepsSize()/resources.displayMetrics.density).toInt()
+        progressStepSizeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val sizeInDp = (progress*resources.displayMetrics.density).toInt()
+                progressStepSizeTV.text = "$sizeInDp dp"
+                cpsStepper.setStepsSize(sizeInDp)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
 
 
     }
 
 
-    private fun showColorPicker() {
+    private fun showColorPicker(initColor: String,completion:(selectedColor:Int) -> Unit) {
         ColorPickerDialogBuilder
             .with(this)
             .setTitle("Choose color")
-            .initialColor(Color.parseColor("#DA5DCA"))
+            .initialColor(Color.parseColor(initColor))
             .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
             .density(12)
             .setOnColorSelectedListener { selectedColor ->
+                completion(selectedColor)
                 Toast.makeText(this,
                     "onColorSelected: 0x" + Integer.toHexString(
                         selectedColor),Toast.LENGTH_LONG
@@ -85,7 +127,9 @@ class MainActivity : AppCompatActivity() {
             }
             .setPositiveButton(
                 "ok"
-            ) { dialog, selectedColor, allColors ->/* changeBackgroundColor(selectedColor)*/ }
+            ) { dialog, selectedColor, allColors ->
+                completion(selectedColor)
+            }
             .setNegativeButton("cancel") { dialog, which -> }
             .build()
             .show()
--- cps/src/main/java/com/taleb/cps/CircularProgressStepper.kt
+++ cps/src/main/java/com/taleb/cps/CircularProgressStepper.kt
@@ -5,22 +5,22 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.graphics.toRect
 import kotlin.math.max
 import kotlin.math.min
 
 
 class CircularProgressStepper : View {
 
-    private var stepsSize = (18*resources.displayMetrics.density).toInt()
-    private var progressColor = Color.parseColor("#30c39e")
+    private var stepsSize = (18 * resources.displayMetrics.density).toInt()
+    private var progressColor = Color.parseColor("#DA5DCA")
     private var foregroundColor = Color.parseColor("#caccd1")
     private var currentProgress = 0f
     private var endProgress = 360f
     private var numOfSteps = 6
     private var stepsImgSrc = emptyArray<Int>()
+    private var speedCount = 2f
     private var currentStep = 1
-    private var progressStrokeOffset = (2*resources.displayMetrics.density).toInt()
+    private var progressStrokeOffset = (2 * resources.displayMetrics.density).toInt()
     //
     private val circlePaint = Paint()
     private val progressPaint = Paint()
@@ -44,10 +44,16 @@ class CircularProgressStepper : View {
             try {
                 numOfSteps = typedArray.getInteger(R.styleable.CircularProgressStepper_cps_numOfSteps, numOfSteps)
                 currentStep = typedArray.getInteger(R.styleable.CircularProgressStepper_cps_currentStep, currentStep)
-                stepsSize= typedArray.getDimensionPixelSize(R.styleable.CircularProgressStepper_cps_stepsSize,stepsSize)
-                foregroundColor = typedArray.getColor(R.styleable.CircularProgressStepper_cps_foregroundColor,foregroundColor)
-                progressColor = typedArray.getColor(R.styleable.CircularProgressStepper_cps_progressColor,progressColor)
-                progressStrokeOffset = typedArray.getDimensionPixelOffset(R.styleable.CircularProgressStepper_cps_progressStrokeOffset,progressStrokeOffset)
+                stepsSize =
+                    typedArray.getDimensionPixelSize(R.styleable.CircularProgressStepper_cps_stepsSize, stepsSize)
+                foregroundColor =
+                    typedArray.getColor(R.styleable.CircularProgressStepper_cps_foregroundColor, foregroundColor)
+                progressColor =
+                    typedArray.getColor(R.styleable.CircularProgressStepper_cps_progressColor, progressColor)
+                progressStrokeOffset = typedArray.getDimensionPixelOffset(
+                    R.styleable.CircularProgressStepper_cps_progressStrokeOffset,
+                    progressStrokeOffset
+                )
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -60,26 +66,21 @@ class CircularProgressStepper : View {
 
         //todo(add infinite progress)
         //todo(add custom image to steps)
-        //todo(when numOfStep is odd steps not draw correctly -> bug)
+        //todo(add progress startStep)
     }
 
     private fun paintsInit() {
         circlePaint.style = Paint.Style.STROKE
-        circlePaint.color = foregroundColor
         circlePaint.isAntiAlias = true
         //
         progressPaint.style = Paint.Style.STROKE
         progressPaint.strokeCap = Paint.Cap.ROUND
-        progressPaint.color = progressColor
         progressPaint.isAntiAlias = true
         //
         stepPaint.style = Paint.Style.FILL
-        stepPaint.color = foregroundColor
         stepPaint.isAntiAlias = true
         //
         stepImgPaint.style = Paint.Style.FILL
-        val filter = PorterDuffColorFilter(progressColor, PorterDuff.Mode.SRC_IN)
-        stepImgPaint.colorFilter = filter
         stepImgPaint.isAntiAlias = true
     }
 
@@ -94,7 +95,7 @@ class CircularProgressStepper : View {
 
     fun setCurrentStep(currentStep: Int) {
         if (currentStep > numOfSteps) {
-            throw java.lang.Exception("current step must be equal or smaller than numOfSteps($numOfSteps)")
+            return
         }
         this.currentStep = currentStep
         if (currentStep == 0) {
@@ -104,19 +105,18 @@ class CircularProgressStepper : View {
             this.endProgress = stepDegree * currentStep
         }
 
-        /*if (currentProgress >= endProgress){
-            this.currentProgress = endProgress
-        }*/
-
         postInvalidate()
     }
 
-    fun setNumOfSeps(numOfSteps: Int){
+    fun setNumOfSteps(numOfSteps: Int) {
         this.numOfSteps = numOfSteps
+        if (currentStep > numOfSteps){
+            currentStep = numOfSteps
+        }
         setCurrentStep(currentStep)
     }
 
-    fun getNumOfSteps(): Int{
+    fun getNumOfSteps(): Int {
         return numOfSteps
     }
 
@@ -125,14 +125,14 @@ class CircularProgressStepper : View {
     }
 
     fun goToNextStep() {
-        if (currentStep+1 < numOfSteps){
+        if (currentStep + 1 < numOfSteps) {
             this.currentStep += 1
             setCurrentStep(currentStep)
         }
     }
 
     fun gotToPrevStep() {
-        if (currentStep > 0){
+        if (currentStep > 0) {
             currentStep -= 1
             setCurrentStep(currentStep)
         }
@@ -140,32 +140,62 @@ class CircularProgressStepper : View {
 
     fun setStepsImgSrc(stepsImgSrc: Array<Int>) {
         this.stepsImgSrc = stepsImgSrc
-        setNumOfSeps(stepsImgSrc.size)
+        setNumOfSteps(stepsImgSrc.size)
         setCurrentStep(currentStep)
     }
 
-    fun setProgressStrokeOffset(progressStrokeOffset: Int){
+    fun setProgressStrokeOffset(progressStrokeOffset: Int) {
         this.progressStrokeOffset = progressStrokeOffset
         postInvalidate()
     }
 
-    fun getProgressStrokeOffset(): Int{
+    fun getProgressStrokeOffset(): Int {
         return progressStrokeOffset
+    }
+
+    fun getStepsSize(): Int {
+        return stepsSize
+    }
+
+    fun setStepsSize(stepsSize: Int) {
+        this.stepsSize = stepsSize
+        postInvalidate()
+    }
+
+    fun getForegroundColor(): Int {
+        return foregroundColor
+    }
+
+    fun setForegroundColor(foregroundColor: Int) {
+        this.foregroundColor = foregroundColor
+        postInvalidate()
+    }
+
+    fun getProgressColor(): Int {
+        return progressColor
+    }
+
+    fun setProgressColor(progressColor: Int) {
+        this.progressColor = progressColor
+        postInvalidate()
     }
 
 
     private fun setupStepper(canvas: Canvas) {
-        val maxPadding = max(max(max(paddingRight,paddingLeft), max(paddingStart,paddingEnd)), max(paddingTop,paddingBottom))
-        val circleRadius = ((min(width, height) / 2.toFloat()) - stepsSize / 2)-maxPadding
-        val circleWidth = min(width, height).toFloat()-2*maxPadding
+        val maxPadding =
+            max(max(max(paddingRight, paddingLeft), max(paddingStart, paddingEnd)), max(paddingTop, paddingBottom))
+        val circleRadius = ((min(width, height) / 2.toFloat()) - stepsSize / 2) - maxPadding
+        val circleWidth = min(width, height).toFloat() - 2 * maxPadding
         val circleStrokeWidth = (stepsSize / 4).toFloat()
-        val progressStrokeWidth = circleStrokeWidth-progressStrokeOffset
+        val progressStrokeWidth = circleStrokeWidth - progressStrokeOffset
 
         //draw background layer
+        circlePaint.color = foregroundColor
         circlePaint.strokeWidth = circleStrokeWidth
         canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), circleRadius, circlePaint)
 
         //draw progress layer
+        progressPaint.color = progressColor
         progressPaint.strokeWidth = progressStrokeWidth
 
         val rectLeft = (width - circleWidth) / 2
@@ -175,15 +205,21 @@ class CircularProgressStepper : View {
         canvas.drawArc(rectProgress, 0f, currentProgress, false, progressPaint)
 
         //draw step circles
-        val stepDegree = 360 / numOfSteps
+        stepPaint.color = foregroundColor
+
+        //draw step img paint
+        val filter = PorterDuffColorFilter(progressColor, PorterDuff.Mode.SRC_IN)
+        stepImgPaint.colorFilter = filter
+
+        val stepDegree = 360.0f / numOfSteps
         for (i in 0 until numOfSteps) {
-            val currentDegree: Float = (stepDegree * i).toFloat()
+            val currentDegree: Float = stepDegree * i
             val theta = (currentDegree * Math.PI) / 180
             val centerX = width / 2 - stepsSize / 2
             val centerY = height / 2 - stepsSize / 2
             val left = (circleRadius * Math.cos(theta) + centerX).toFloat()
             val top = (circleRadius * Math.sin(theta) + centerY).toFloat()
-            if (currentDegree <= currentProgress) {
+            if (currentDegree <= currentProgress+speedCount) {
                 stepPaint.color = progressColor
                 val filter1 = PorterDuffColorFilter(foregroundColor, PorterDuff.Mode.SRC_IN)
                 stepImgPaint.colorFilter = filter1
@@ -196,16 +232,19 @@ class CircularProgressStepper : View {
             canvas.drawOval(rectF, stepPaint)
             //
             if (stepsImgSrc.size > i) {
-                val bitmap = BitmapFactory.decodeResource(resources,stepsImgSrc[i])
-                canvas.drawBitmap(bitmap,null,rectF,stepImgPaint)
+                val bitmap = BitmapFactory.decodeResource(resources, stepsImgSrc[i])
+                val newRectImg = rectF
+                val imgPaddingOffset = stepsSize*0.1f
+                newRectImg.inset(imgPaddingOffset,imgPaddingOffset)
+                canvas.drawBitmap(bitmap, null, newRectImg, stepImgPaint)
             }
         }
 
-        if (currentProgress < endProgress) {
-            currentProgress += 2.0f
+        if (currentProgress+speedCount < endProgress) {
+            currentProgress += speedCount
             postInvalidate()
-        }else if (currentProgress > endProgress){
-            currentProgress -= 2.0f
+        } else if (currentProgress-speedCount > endProgress) {
+            currentProgress -= speedCount
             postInvalidate()
         }
     }
